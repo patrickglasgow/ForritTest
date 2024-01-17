2006bf3756dcb2c8e1f178ba995808e4b19d8b24
Patrick Kelly <patrickglasgow1992@googlemail.com>
2024-01-17 08:30:56 +0000

    Loading from localstorage on load of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import * as dayjs from 'dayjs';
 import { DateService } from './services/date/date.service';
+import { EventService } from './services/event/event.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,10 @@ import { DateService } from './services/date/date.service';
 })
 export class AppComponent {
 
-  constructor(private dateService: DateService) {}
+  constructor(private dateService: DateService, private eventService: EventService) {}
 
   ngOnInit() {
-    this.dateService.setCurrentDate(dayjs())
+    this.dateService.setCurrentDate(dayjs());
+    this.eventService.loadEventsFromLocalStorage();
   }
 }
